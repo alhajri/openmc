13b22ca9fd0c3e258163096c443e25924b86552c
Abdulla Alhajri <alhajri@mit.edu>
2020-04-22 16:43:26 +0400

    changed SensitivityTallyValue to TallyValue

--- src/tallies/tally_scoring.cpp
+++ src/tallies/tally_scoring.cpp
@@ -2335,7 +2335,7 @@ void score_collision_sensitivity_tally(Particle* p, int i_tally, int start_index
     // Update tally results
     for (auto idx = 0; idx < cumulative_sensitivities.size(); idx++){
       #pragma omp atomic
-      tally.results_(idx, score_index, SensitivityTallyResult::VALUE) += cumulative_sensitivities[idx]*score*filter_weight;
+      tally.results_(idx, score_index, TallyResult::VALUE) += cumulative_sensitivities[idx]*score*filter_weight;
     }
     if (sens.sens_reaction == SCORE_FISSION || sens.sens_reaction == SCORE_TOTAL){
       // Get the energy of the parent particle.
